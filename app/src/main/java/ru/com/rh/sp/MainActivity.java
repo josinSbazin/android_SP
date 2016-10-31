c103c9982f892847d10d8123b0bedc8ad77069ca
rhanza <rhanza@bk.ru>
2016-10-31 03:39:02 +0300

    Добавил кнопку и задал метод для ее нажатия (открывание Drawer)

--- app/src/main/java/ru/com/rh/sp/MainActivity.java
+++ app/src/main/java/ru/com/rh/sp/MainActivity.java
@@ -3,8 +3,12 @@ package ru.com.rh.sp;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private View mDrawerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //disable shadow on DrawerMenu
-        ((DrawerLayout)findViewById(R.id.drawer_layout)).setDrawerElevation(0);
+        mDrawerLayout = ((DrawerLayout)findViewById(R.id.drawer_layout));
+        mDrawerView = findViewById(R.id.left_drawer);
+        mDrawerLayout.setDrawerElevation(0);
+    }
+
+    public void onClickNavigateButton(View view) {
+        if (mDrawerLayout != null)
+        mDrawerLayout.openDrawer(mDrawerView);
     }
 }
