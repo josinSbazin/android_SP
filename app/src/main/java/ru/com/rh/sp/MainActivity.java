3c6bc337129d2f364e281b5b705fe703f8c8b77b
rhanza <rhanza@bk.ru>
2016-11-09 01:00:56 +0300

    Добавил меню ( с телефонами)

--- app/src/main/java/ru/com/rh/sp/MainActivity.java
+++ app/src/main/java/ru/com/rh/sp/MainActivity.java
@@ -5,10 +5,38 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    // названия компаний (групп)
+    String[] groups = new String[] {"HTC", "Samsung", "LG"};
+
+    // названия телефонов (элементов)
+    String[] phonesHTC = new String[] {"Sensation", "Desire", "Wildfire", "Hero"};
+    String[] phonesSams = new String[] {"Galaxy S II", "Galaxy Nexus", "Wave"};
+    String[] phonesLG = new String[] {"Optimus", "Optimus Link", "Optimus Black", "Optimus One"};
+
+    // коллекция для групп
+    ArrayList<Map<String, String>> groupData;
+
+    // коллекция для элементов одной группы
+    ArrayList<Map<String, String>> childDataItem;
+
+    // общая коллекция для коллекций элементов
+    ArrayList<ArrayList<Map<String, String>>> childData;
+    // в итоге получится childData = ArrayList<childDataItem>
+
+    // список атрибутов группы или элемента
+    Map<String, String> m;
+
+    ExpandableListView elvMain;
 
     private DrawerLayout mDrawerLayout;
     private View mDrawerView;
@@ -22,6 +50,73 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout = ((DrawerLayout)findViewById(R.id.drawer_layout));
         mDrawerView = findViewById(R.id.left_drawer);
         initDrawer(mDrawerLayout);
+
+
+        // заполняем коллекцию групп из массива с названиями групп
+        groupData = new ArrayList<Map<String, String>>();
+        for (String group : groups) {
+            // заполняем список атрибутов для каждой группы
+            m = new HashMap<String, String>();
+            m.put("groupName", group); // имя компании
+            groupData.add(m);
+        }
+
+        // список атрибутов групп для чтения
+        String groupFrom[] = new String[] {"groupName"};
+        // список ID view-элементов, в которые будет помещены атрибуты групп
+        int groupTo[] = new int[] {android.R.id.text1};
+
+
+        // создаем коллекцию для коллекций элементов
+        childData = new ArrayList<ArrayList<Map<String, String>>>();
+
+        // создаем коллекцию элементов для первой группы
+        childDataItem = new ArrayList<Map<String, String>>();
+        // заполняем список атрибутов для каждого элемента
+        for (String phone : phonesHTC) {
+            m = new HashMap<String, String>();
+            m.put("phoneName", phone); // название телефона
+            childDataItem.add(m);
+        }
+        // добавляем в коллекцию коллекций
+        childData.add(childDataItem);
+
+        // создаем коллекцию элементов для второй группы
+        childDataItem = new ArrayList<Map<String, String>>();
+        for (String phone : phonesSams) {
+            m = new HashMap<String, String>();
+            m.put("phoneName", phone);
+            childDataItem.add(m);
+        }
+        childData.add(childDataItem);
+
+        // создаем коллекцию элементов для третьей группы
+        childDataItem = new ArrayList<Map<String, String>>();
+        for (String phone : phonesLG) {
+            m = new HashMap<String, String>();
+            m.put("phoneName", phone);
+            childDataItem.add(m);
+        }
+        childData.add(childDataItem);
+
+        // список атрибутов элементов для чтения
+        String childFrom[] = new String[] {"phoneName"};
+        // список ID view-элементов, в которые будет помещены атрибуты элементов
+        int childTo[] = new int[] {android.R.id.text1};
+
+        SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+                this,
+                groupData,
+                android.R.layout.simple_expandable_list_item_1,
+                groupFrom,
+                groupTo,
+                childData,
+                android.R.layout.simple_list_item_1,
+                childFrom,
+                childTo);
+
+        elvMain = (ExpandableListView) findViewById(R.id.menu_expListView);
+        elvMain.setAdapter(adapter);
     }
 
     //Навигационные кнопки внутри и снаружи drawer'a
@@ -44,11 +139,4 @@ public class MainActivity extends AppCompatActivity {
             drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
         }
     }
-
-/*    //Создание menu в drawer'e
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_drawer_main, menu);
-        return true;
-    }*/
 }
