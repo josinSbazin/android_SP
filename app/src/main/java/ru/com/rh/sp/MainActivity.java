0b000ed9ca7397c4838478ae53b71aa76890b670
rhanza <rhanza@bk.ru>
2016-11-14 02:00:12 +0300

    Подправил bouild.gradle, не работает предварительная визуализация в SDK, говорят - баг. Скоро исправят. TODO: Доделать, наконец, правое меню!

--- app/src/main/java/ru/com/rh/sp/MainActivity.java
+++ app/src/main/java/ru/com/rh/sp/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
